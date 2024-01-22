d5b1f2cc94a40d34a1c4f8d6b90787a24d1ff312
edvinmujanovic <edvin.mujanovic@gmail.com>
2024-01-22 21:22:29 +0100

    Initial Commit

---
+++ WeatherAppiOS/WeatherAppiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppiOSApp.swift
+//  WeatherAppiOS
+//
+//  Created by Edvin Mujanovic on 2024-01-22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
